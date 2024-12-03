7370022cdc4639e730b85650fcca20a32de389ba
yana.zhmak <ms.panzhina@gmail.com>
2024-12-03 17:51:56 -0500

    Initial commit

---
+++ src/main/java/com/example/spring_render_api/SpringRenderApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_render_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRenderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRenderApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_render_api/SpringRenderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_render_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRenderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
