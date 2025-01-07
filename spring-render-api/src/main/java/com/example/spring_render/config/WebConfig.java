6109081842f81cfc942fff8740e5ab04c6ee8f8d
yana.zhmak <ms.panzhina@gmail.com>
2025-01-07 15:38:23 -0500

    created WebConfig class to handle CORS

---
+++ spring-render-api/src/main/java/com/example/spring_render/config/WebConfig.java
@@ -0,0 +1,28 @@
+package com.example.spring_render.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Created by yana.zhmak on 06.01.2025.
+ */
+
+@Configuration
+public class WebConfig {
+
+    // Handle CORS (Cross-Origin Resource Sharing)
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://notesofawoman.netlify.app")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
